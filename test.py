6363bd4d069ed611ac35028a7e9721f85ee37e24
2004240023 <2004240023yr@edu.oca.ac.jp>
2022-05-07 17:43:53 +0900

    if,for 追加

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
+from ast import If
+
+
 def main():
     print('Hello Python')
 
 if __name__=='__main__':
     main()
+
+def a(b):
+    return  b
+
+num = int(input('整数を入力してね＞'))
+if num % 2 == 0:
+     print('偶数')
+else:
+     print('奇数')
+
+for num in range(1, 10):
+    print(num)
